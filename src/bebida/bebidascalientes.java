8dcee8f4163959ccbf5cc1454a428a8758ced471
jossanchezv <vargas3502009@gmail.com>
2018-04-12 07:25:19 -0500

    clases hasta ahora

---
+++ src/bebida/bebidascalientes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bebida;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class bebidascalientes extends bebidas{
+
+    @Override
+    public void seleccion() {
+        int op;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que bebida desea?");
+       switch ()
+    }
+
+    
+}
